7adc22c594ba1e9f778f8bc34bbe055241a02d30
FinerKun <stefanusderren@gmail.com>
2025-03-17 19:28:12 +0800

    10

--- components/store/HeroSection.tsx
+++ components/store/HeroSection.tsx
@@ -11,14 +11,6 @@ export function HeroSection() {
         alt="Picture of the author"
         className="absolute overflow-clip object-cover"
       />
-      <div className="z-10 flex max-h-[1920px] w-full flex-col items-center justify-center px-6 pb-24 tracking-wide">
-        <p className="text-center text-2xl text-[#F5EEC5] md:text-4xl">
-          WELCOME TO
-        </p>
-        <h1 className="my-4 -rotate-[3deg] bg-[#F5EEC5] px-4 py-2 text-center text-4xl font-bold text-[#892217] md:text-6xl">
-          EKRAF&apos;S THINGS
-        </h1>
-      </div>
     </section>
   );
 }
